770495382e3da25395180352d73987f325a9c774
Breno Brito <breno.silvabrito@hotmail.com>
2022-10-20 09:35:33 +0100

    Connect /image url(users image entries) to the database

--- server.js
+++ server.js
@@ -90,17 +90,15 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const {id} = req.body;
-    let found = false;
-    db.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.entries++
-            res.json(user.entries);
-        }
-    })
-    if (!found) {
-        res.status(404).json('not found');
-    }
+    database('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries)
+        })
+        .catch(err => {
+            res.status(400).json('unable to get entries');
+        })
 })
 
 app.listen(5000, () => {
